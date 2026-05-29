716e1f5c657e08d4a215902af55b507afe238b09
Lu <luciaaragonvaello@gmail.com>
2026-05-30 01:43:23 +0200

    Protect write API routes with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/posts', [ApiBlogController::class, 'index']);
 Route::get('/posts/{post}', [ApiBlogController::class, 'show']);
-Route::post('/posts', [ApiBlogController::class, 'store']);
-Route::put('/posts/{post}', [ApiBlogController::class, 'update']);
-Route::delete('/posts/{post}', [ApiBlogController::class, 'destroy']);
-Route::post('/posts/{post}/comments', [ApiBlogController::class, 'addComment']);
+
+Route::middleware('auth:sanctum')->group(function (): void {
+    Route::post('/posts', [ApiBlogController::class, 'store']);
+    Route::put('/posts/{post}', [ApiBlogController::class, 'update']);
+    Route::delete('/posts/{post}', [ApiBlogController::class, 'destroy']);
+    Route::post('/posts/{post}/comments', [ApiBlogController::class, 'addComment']);
+});
