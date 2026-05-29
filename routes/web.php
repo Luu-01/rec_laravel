f2801ad441ba3b29cab85a1fddc643b362ebae8e
Lu <luciaaragonvaello@gmail.com>
2026-05-30 00:18:44 +0200

    Register web blog routes

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,10 @@ use App\Http\Controllers\WebPostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => redirect()->route('posts.index'));
-
 Route::get('/register', [AuthSessionController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthSessionController::class, 'register'])->name('register.store');
 Route::get('/login', [AuthSessionController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthSessionController::class, 'login'])->name('login.store');
 Route::post('/logout', [AuthSessionController::class, 'logout'])->middleware('auth')->name('logout');
-
 Route::resource('posts', WebPostController::class);
+Route::post('/posts/{post}/comments', [WebPostController::class, 'addComment'])->name('posts.comments.store');
