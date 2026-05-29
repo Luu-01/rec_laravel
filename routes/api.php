621c8939901bc06b3f9feac128792af7d6d41127
Lu <luciaaragonvaello@gmail.com>
2026-05-30 00:42:45 +0200

    Register API blog routes

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,11 @@
 <?php
 
-// REST API route file.
+use App\Http\Controllers\Api\ApiBlogController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/posts', [ApiBlogController::class, 'index']);
+Route::get('/posts/{post}', [ApiBlogController::class, 'show']);
+Route::post('/posts', [ApiBlogController::class, 'store']);
+Route::put('/posts/{post}', [ApiBlogController::class, 'update']);
+Route::delete('/posts/{post}', [ApiBlogController::class, 'destroy']);
+Route::post('/posts/{post}/comments', [ApiBlogController::class, 'addComment']);
