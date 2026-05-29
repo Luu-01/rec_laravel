a15b1ab055a7ae65f51aea0bb5cc6b8d3a8d7c63
Lu <luciaaragonvaello@gmail.com>
2026-05-29 23:57:59 +0200

    Register web authentication and post routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthSessionController;
+use App\Http\Controllers\WebPostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => 'ok');
+Route::get('/', fn () => redirect()->route('posts.index'));
+
+Route::get('/register', [AuthSessionController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [AuthSessionController::class, 'register'])->name('register.store');
+Route::get('/login', [AuthSessionController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthSessionController::class, 'login'])->name('login.store');
+Route::post('/logout', [AuthSessionController::class, 'logout'])->middleware('auth')->name('logout');
+
+Route::resource('posts', WebPostController::class);
